20693a3ca0943819f23a9077814e0a33f8b90f72
Joswin Dsouza <dsouzajoswin99@gmail.com>
2023-05-31 22:20:25 +0530

    With  API Connection

--- src/User Info/UserDetails.tsx
+++ src/User Info/UserDetails.tsx
@@ -1,43 +1,91 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
+
+interface Users {
+  id: number;
+  username: string;
+  companyID: string;
+  companyName: string;
+  usertype: string;
+}
+
 const UserDetails = () => {
   const navigate = useNavigate();
   const goDetails = () => {
     navigate("/showuserdetails/e001");
   };
-  let users = [
-    {
-      UID: "e001",
-      name: "Tom",
-      CompanyID: "007",
-      CompanyName: "Infosys",
-      UserType: "Main",
-    },
-    {
-      UID: "e002",
-      name: "Jude",
-      CompanyID: "007",
-      CompanyName: "Trivium",
-      UserType: "Main",
-    },
-    {
-      UID: "e003",
-      name: "Rudy",
-      CompanyID: "007",
-      CompanyName: "TCS",
-      UserType: "Main",
-    },
-    {
-      UID: "e004",
-      name: "Bond",
-      CompanyID: "007",
-      CompanyName: "Wipro",
-      UserType: "Main",
-    },
-  ];
+  const [users, setUsers] = useState<Users[]>([]);
+  const [error, setErrors] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5192/admin") //success  of APi Call
+      .then((res) => setUsers(res.data)) //if any error or failed the APi call to the Server
+      .catch((err) => {
+        setErrors(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  const deleteUser = (user: Users) => {
+    const orginalState = [...users];
+    setUsers(users.filter((x) => x.id !== user.id));
+    axios.delete("http://localhost:5192/admin" + user.id).catch((err) => {
+      setErrors(err.message);
+      setUsers(orginalState);
+    });
+  };
+  const addUser = () => {
+    const newUser = {
+      id: 1,
+      username: "Joswin",
+      companyID: "Triv007",
+      companyName: "KPMG",
+      usertype: "User",
+    };
+    setUsers([...users, newUser]);
+    axios
+      .post("http://localhost:5192/admin", newUser)
+      .then((res) => setUsers([...users, res.data]));
+  };
+
+  // let users = [
+  //   {
+  //     UID: "e001",
+  //     name: "Tom",
+  //     CompanyID: "007",
+  //     CompanyName: "Infosys",
+  //     UserType: "Main",
+  //   },
+  //   {
+  //     UID: "e002",
+  //     name: "Jude",
+  //     CompanyID: "007",
+  //     CompanyName: "Trivium",
+  //     UserType: "Main",
+  //   },
+  //   {
+  //     UID: "e003",
+  //     name: "Rudy",
+  //     CompanyID: "007",
+  //     CompanyName: "TCS",
+  //     UserType: "Main",
+  //   },
+  //   {
+  //     UID: "e004",
+  //     name: "Bond",
+  //     CompanyID: "007",
+  //     CompanyName: "Wipro",
+  //     UserType: "Main",
+  //   },
+  // ];
 
   return (
     <>
+      {error && <p>{error}</p>}     
+      {isLoading && <div className="spiner-border">Loading....</div>}   
       <table className="table table-striped table-bordered">
         <thead className="thead-dark">
           <tr>
@@ -49,21 +97,21 @@ const UserDetails = () => {
           </tr>
         </thead>
         <tbody>
-          {users.map((item, index) => (
-            <tr key={index}>
-              <td>{item.UID}</td>
-              <td>{item.name}</td>
-              <td>{item.CompanyID}</td>
-              <td>{item.CompanyName}</td>
-              <td>{item.UserType}</td>
+          {users.map((index) => (
+            <tr key={index.id}>
+              <td>{index.id}</td>
+              <td>{index.username}</td>
+              <td>{index.companyID}</td>
+              <td>{index.companyName}</td>
+              <td>{index.usertype}</td>
               <td>
-                <button onClick={goDetails}>Details</button>
+                <button onClick={() => deleteUser(index)}>Delete</button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      <button onClick={() => navigate("/adduser/")}> Add User</button>
+      <button onClick={addUser}>Add User</button>   
     </>
   );
 };
